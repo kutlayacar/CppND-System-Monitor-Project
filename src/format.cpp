3808e82341b7833780d748cdd4341d75c4609c9c
br33x <acar.ismailkutlay@gmail.com>
2022-02-13 15:29:13 -0800

    Elapsed Time

--- src/format.cpp
+++ src/format.cpp
@@ -8,4 +8,19 @@ using std::string;
 // INPUT: Long int measuring seconds
 // OUTPUT: HH:MM:SS
 // REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+string Format::ElapsedTime(long seconds) { 
+    
+    // Calculate the hours, minutes and remaining seconds
+    long hours, mins, secs;
+    hours = seconds / 3600;
+    mins = (seconds - hours * 3600) / 60;
+    secs = (seconds - hours * 3600) % 60;
+
+    string HH, MM, SS;
+    // Convert the values to string
+    HH = hours < 10 ? "0" + std::to_string(hours) : std::to_string(hours);
+    MM = mins < 10 ? "0" + std::to_string(mins) : std::to_string(mins);
+    SS = secs < 10 ? "0" + std::to_string(secs) : std::to_string(secs);
+
+    return HH + ":" + MM + ":" + SS;
+}
